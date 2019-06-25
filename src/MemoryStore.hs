2b3a41d6a913bbf18d339eb3f1dd74ece9169d06
Simon Pettersson <simon.v.pettersson@gmail.com>
2019-06-25 23:26:19 +0200

    Working source cache

--- src/MemoryStore.hs
+++ src/MemoryStore.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module MemoryStore where
 
 import Data.ByteString
+import Data.ByteString.Char8 as C8
+import Control.Monad.IO.Class
 
 data Error
-  = NetworkError ByteString
+  = BackendError ByteString
   | NotFoundError ByteString
   deriving (Show)
-  
 
 class SourceStore a where
-  getSource :: a -> ByteString -> IO(Either Error ByteString)
+  getSource :: a -> FilePath -> IO(Either Error ByteString)
+  setSource :: a -> FilePath -> IO(Either Error Bool)
--- src/RedisStore.hs
+++ src/RedisStore.hs
@@ -3,16 +3,26 @@
 module RedisStore where
 
 import MemoryStore
-import Database.Redis
-import Control.Monad.IO.Class
 import Data.ByteString
+import Control.Monad.IO.Class
+import Database.Redis
+import Data.ByteString.Char8 as C8
 
 instance SourceStore Connection where
-  getSource conn key = runRedis conn $ do
-    value <- get key
-    return $ case value of
-      Left _ -> Left (NetworkError "HELLO")
-      Right ok -> case ok of
-        Nothing -> Left (NotFoundError key)
-        Just y -> Right y
-  
+  getSource conn filepath = runRedis conn $ do
+    let filename = C8.pack filepath
+    result <- hget "src" filename
+    return $ case result of
+      Left err -> Left $ BackendError $ C8.pack $ show err
+      Right Nothing -> Left $ NotFoundError filename
+      Right (Just x) -> Right x
+
+  setSource conn filepath = runRedis conn $ do
+    let filename = C8.pack filepath
+    content <- liftIO $ Data.ByteString.readFile filepath
+    liftIO $ Data.ByteString.putStrLn content
+    result <- hset "src" filename content
+    return $ case result of
+      Left err -> Left(BackendError $ C8.pack $ show err)
+      Right False -> Left $ BackendError "Insertion failed"
+      Right True -> Right True
